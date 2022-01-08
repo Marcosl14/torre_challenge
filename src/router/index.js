4f2fa2e2f4dbde3ab6bceeec630245f7f91165c8
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-08 17:31:21 -0300

    Add stength to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import User from "../views/User.vue";
+import Strength from "../views/Strength.vue";
 
 const routes = [
   {
-    path: "/:username",
+    path: "/user/:username",
     name: "User",
     props: true,
     component: User,
+  },
+  {
+    path: "/strength/:username/:id",
+    name: "Strength",
+    props: true,
+    component: Strength,
   },
 ];
 
