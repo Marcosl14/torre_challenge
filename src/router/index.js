5df043b4558234e3f96593923b95ec7db4ee53f5
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-08 08:37:55 -0300

    Rename home view to user view

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import User from "../views/User.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "User",
+    component: User,
   },
 ];
 
