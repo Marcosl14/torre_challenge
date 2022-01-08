707132608a6287025607620df4e893723e04e708
Marcos Giordano <marcos_l14@hotmail.com>
2022-01-08 17:30:20 -0300

    Add all proxies to torre api to prevent cors blocking

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
-// vue.config.js
 module.exports = {
   devServer: {
-    proxy: "https://bio.torre.co/",
+    proxy: {
+      "/get1": {
+        target: "https://bio.torre.co/api/",
+        changeOrigin: true,
+        pathRewrite: { "^/get1": "" },
+      },
+      "/get2": {
+        target: "https://torre.co/api/",
+        changeOrigin: true,
+        pathRewrite: { "^/get2": "" },
+      },
+      "/post": {
+        target: "https://search.torre.co",
+        changeOrigin: true,
+        pathRewrite: { "^/post": "" },
+      },
+    },
   },
 };
